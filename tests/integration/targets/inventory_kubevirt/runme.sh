8fae3cdb9a5ba6e0da286af1d3409c1a3855fe1d
Guido Grazioli <ggraziol@redhat.com>
2023-07-13 15:17:20 +0200

    update inventory inttest

--- tests/integration/targets/inventory_kubevirt/runme.sh
+++ tests/integration/targets/inventory_kubevirt/runme.sh
@@ -2,58 +2,23 @@
 
 set -eux
 
-source virtualenv.sh
-pip install kubernetes PyYAML jsonpatch Jinja2
+export ANSIBLE_ROLES_PATH="../"
+USER_CREDENTIALS_DIR=$(pwd)
 
-./server.py &
+{
+export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
+export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
+export ANSIBLE_PYTHON_INTERPRETER=auto_silent
 
-cleanup() {
-  kill -9 "$(jobs -p)"
-}
-
-trap cleanup INT TERM EXIT
-
-# Fake auth file
-mkdir -p ~/.kube/
-cat <<EOF > ~/.kube/config
-apiVersion: v1
-clusters:
-- cluster:
-    insecure-skip-tls-verify: true
-    server: http://localhost:12345
-  name: development
-contexts:
-- context:
-    cluster: development
-    user: developer
-  name: dev-frontend
-current-context: dev-frontend
-kind: Config
-preferences: {}
-users:
-- name: developer
-  user:
-    token: ZDNg7LzSlp8a0u0fht_tRnPMTOjxqgJGCyi_iy0ecUw
-EOF
-
-#################################################
-#   RUN THE PLUGIN
-#################################################
-
-# run the plugin second
-export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt
-
-cat << EOF > "$OUTPUT_DIR/test.kubevirt.yml"
+cat << EOF > "test.kubevirt.yml"
 plugin: kubernetes.kubevirt.kubevirt
 connections:
   - namespaces:
     - default
 EOF
 
-ansible-inventory -vvvv -i "$OUTPUT_DIR/test.kubevirt.yml" --list --output="$OUTPUT_DIR/plugin.out"
+ansible-inventory -i test.kubevirt.yml -vvv --list "$@"
 
-#################################################
-#   DIFF THE RESULTS
-#################################################
-
-diff "$(pwd)/test.out" "$OUTPUT_DIR/plugin.out"
+} || {
+    exit 1
+}
