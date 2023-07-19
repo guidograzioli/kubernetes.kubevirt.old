80f04fb6e74889000131dc130110196efc9e2c18
Guido Grazioli <ggraziol@redhat.com>
2023-07-19 19:00:27 +0200

    temporarily skip tests

--- tests/integration/targets/kubevirt_vm/runme.sh
+++ tests/integration/targets/kubevirt_vm/runme.sh
@@ -2,9 +2,11 @@
 set -eux
 export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
 export ANSIBLE_ROLES_PATH=../
+export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
+
 
 mkdir files
 ssh-keygen -t ed25519 -C test@test -f files/priv_key
 ssh-keygen -y -f priv_key > files/pub_key
 
-ansible-playbook playbook.yml "$@"
+ansible-playbook playbook.yml --private-key=files/priv_key $@"
