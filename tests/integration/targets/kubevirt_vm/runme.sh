106f056ee21c2e1554211a6808010ef6eab1667b
Guido Grazioli <ggraziol@redhat.com>
2023-07-20 15:06:45 +0200

    typo

--- tests/integration/targets/kubevirt_vm/runme.sh
+++ tests/integration/targets/kubevirt_vm/runme.sh
@@ -6,9 +6,9 @@ export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
 export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
 
 [ -d files ] || mkdir files
-[ -f files/priv_key ] || (ssh-keygen -t ed25519 -C test@test -f files/priv_key ; ssh-keygen -y -f priv_key > files/pub_key)
+[ -f files/priv_key ] || (ssh-keygen -t ed25519 -C test@test -f files/priv_key ; ssh-keygen -y -f files/priv_key > files/pub_key)
 
-ansible-playbook playbook.yml -vvv --private-key=files/priv_key "$@"
+ansible-playbook playbook.yml -vvv  "$@"
 
 ansible-inventory -i test.kubevirt.yml -y -vvv --list "$@"
 
