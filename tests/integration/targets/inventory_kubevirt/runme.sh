dade13e36409ea5a4d19d0110dfbc3b39f5a2fbe
Guido Grazioli <ggraziol@redhat.com>
2023-07-20 17:30:42 +0200

    typo

--- tests/integration/targets/inventory_kubevirt/runme.sh
+++ tests/integration/targets/inventory_kubevirt/runme.sh
@@ -12,15 +12,17 @@ export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
 export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
 export ANSIBLE_PYTHON_INTERPRETER=auto_silent
 
-ansible-inventory -i test.kubevirt.yml -y -vvv --list "$@"
+ansible-inventory -i test.kubevirt.yml -y --list --output empty.yml "$@"
 
 ansible-playbook -vvv playbook.yml
 
 # should list 2 vmi
-ansible-inventory -i test.kubevirt.yml -y -vvv --list "$@"
+ansible-inventory -i test.kubevirt.yml -y -vvv --list --output all.yml "$@"
 
 # should list 1 vm with label_app_test
-ansible-inventory -i test.label.kubevirt.yml -y -vvv --list "$@"
+ansible-inventory -i test.label.kubevirt.yml -y -vvv --list --output label.yml "$@"
+
+ansible-playbook -vvv verify.yml
 
 } || {
     exit 1
