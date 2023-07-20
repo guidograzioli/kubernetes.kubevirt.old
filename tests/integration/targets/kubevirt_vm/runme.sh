a1f1921fa66e5df43b3771dfdd4f47309c659c9e
Guido Grazioli <ggraziol@redhat.com>
2023-07-20 13:09:56 +0200

    get rib of turbo mode

--- tests/integration/targets/kubevirt_vm/runme.sh
+++ tests/integration/targets/kubevirt_vm/runme.sh
@@ -6,7 +6,6 @@ export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
 
 
 [ -d files ] || mkdir files
-ssh-keygen -t ed25519 -C test@test -f files/priv_key
-ssh-keygen -y -f priv_key > files/pub_key
+[ -f files/priv_key ] || (ssh-keygen -t ed25519 -C test@test -f files/priv_key ; ssh-keygen -y -f priv_key > files/pub_key)
 
 ansible-playbook playbook.yml --private-key=files/priv_key $@"
