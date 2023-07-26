c03fc363ad3a570766f1538b26742180f16be6c0
Guido Grazioli <guido.grazioli@gmail.com>
2023-07-26 09:40:53 +0200

    integration tests update (#1)
    * integration tests update

---
+++ tests/integration/targets/kubevirt_vm/runme.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eux
+set -o pipefail
+
+{
+export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
+export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
+
+[ -d files ] || mkdir files
+[ -f files/priv_key ] || (ssh-keygen -t ed25519 -C test@test -f files/priv_key ; ssh-keygen -y -f files/priv_key > files/pub_key)
+
+ip a
+route -n
+
+ansible-playbook playbook.yml "$@"
+
+ansible-inventory -i test.kubevirt.yml -y --list "$@"
+
+ansible-playbook verify.yml -i test.kubevirt.yml --private-key=files/priv_key "$@"
+
+} || {
+    exit 1
+}
