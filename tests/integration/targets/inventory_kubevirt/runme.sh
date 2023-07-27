ff1d5f14acb6062079b661566dad610743c15196
Guido Grazioli <ggraziol@redhat.com>
2023-07-27 10:57:01 +0200

    polish

--- tests/integration/targets/inventory_kubevirt/runme.sh
+++ tests/integration/targets/inventory_kubevirt/runme.sh
@@ -12,20 +12,12 @@ export ANSIBLE_CALLBACKS_ENABLED=profile_tasks
 export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
 export ANSIBLE_PYTHON_INTERPRETER=auto_silent
 
-ip a
-route -n
-
 ansible-inventory -i test.kubevirt.yml -y --list --output empty.yml "$@"
 
-ansible-playbook playbook.yml
+ansible-playbook playbook.yml "$@"
 
-# should list 2 vmi
 ansible-inventory -i test.kubevirt.yml -y --list --output all.yml "$@"
-
-# should list 1 vm with label_app_test
 ansible-inventory -i test.label.kubevirt.yml -y --list --output label.yml "$@"
-
-# should list 1 vm with multus interface
 ansible-inventory -i test.net.kubevirt.yml -y --list --output net.yml "$@"
 
 ansible-playbook verify.yml "$@"
--- tests/integration/targets/kubevirt_vm/runme.sh
+++ tests/integration/targets/kubevirt_vm/runme.sh
@@ -9,9 +9,6 @@ export ANSIBLE_INVENTORY_ENABLED=kubernetes.kubevirt.kubevirt,yaml
 [ -d files ] || mkdir files
 [ -f files/priv_key ] || (ssh-keygen -t ed25519 -C test@test -f files/priv_key ; ssh-keygen -y -f files/priv_key > files/pub_key)
 
-ip a
-route -n
-
 ansible-playbook playbook.yml "$@"
 
 ansible-inventory -i test.kubevirt.yml -y --list "$@"
